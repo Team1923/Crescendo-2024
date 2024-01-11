65f59f857c8fc51a3bf3ad6fa099dfbbd422347c
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2024-01-11 09:09:36 -0500

    Custom PositionRPMObject

---
+++ src/main/java/frc/robot/StateUtils/PositionRPMObject.java
@@ -0,0 +1,43 @@
+package frc.robot.StateUtils;
+
+public class PositionRPMObject {
+    private double armPosition;
+    private double shooterRPM;
+
+    public PositionRPMObject(double armPosition, double shooterRPM) {
+        this.armPosition = armPosition;
+        this.shooterRPM = shooterRPM;
+    }
+
+    /**
+     * Method to retrieve the "armPosition" object.
+     * @return the position of the arm to be commanded.
+     */
+    public double getArmPosition() {
+        return armPosition;
+    }
+
+    /**
+     * Method to retrieve the "shooterRPM" object.
+     * @return the RPM of the shooter to be commanded.
+     */
+    public double getShooterRPM() {
+        return shooterRPM;
+    }
+
+    /**
+     * Helper method to set the position value of the arm.
+     * @param armPosition the desired arm position to set.
+     */
+    public void setArmPosition(double armPosition) {
+        this.armPosition = armPosition;
+    }
+
+    /**
+     * Helper method to set the RPM value of the shooter.
+     * @param shooterRPM the desired shooter RPM to set.
+     */
+    public void setShooterRPM(double shooterRPM) {
+        this.shooterRPM = shooterRPM;
+    }
+}
