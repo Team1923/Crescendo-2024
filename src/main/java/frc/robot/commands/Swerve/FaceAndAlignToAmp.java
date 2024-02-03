df113fcd7f70a2ea302e0f97c19ffe35ff03ab89
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2024-02-03 13:03:17 -0500

    strange redhat issue

--- src/main/java/frc/robot/commands/Swerve/FaceAndAlignToAmp.java
+++ src/main/java/frc/robot/commands/Swerve/FaceAndAlignToAmp.java
@@ -15,14 +15,14 @@ import frc.robot.subsystems.SwerveSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class FaceAndAllignAmp extends SequentialCommandGroup {
+public class FaceAndAlignToAmp extends SequentialCommandGroup {
 
   LimelightInterface ll = LimelightInterface.getInstance();
 
   private final double tolerance = 1; //degree
 
   /** Creates a new PosAndAllignAmp. */
-  public FaceAndAllignAmp(SwerveSubsystem s, DoubleSupplier transSup, DoubleSupplier strafeSup, DoubleSupplier rotSup) {
+  public FaceAndAlignToAmp(SwerveSubsystem s, DoubleSupplier transSup, DoubleSupplier strafeSup, DoubleSupplier rotSup) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
