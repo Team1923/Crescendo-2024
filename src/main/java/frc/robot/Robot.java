24da1cfca95d24fb8b1b0a978923277fa826dc8f
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2024-02-07 17:49:59 -0500

    commented out limelight thing for now

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -38,9 +38,9 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
     // this.selector = new AutoChooser();
-        for (int port = 5800; port <= 5807; port++) {
-            PortForwarder.add(port, "limelight.local", port);
-        }
+    // for (int port = 5800; port <= 5807; port++) {
+    //     PortForwarder.add(port, "limelight.local", port);
+    // }
   }
 
   /**
