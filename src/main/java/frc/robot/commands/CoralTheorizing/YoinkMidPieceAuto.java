cc972016dd9a704c4180cad89e50702c5c090898
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2024-02-07 11:33:26 -0500

    more commenting out

--- src/main/java/frc/robot/commands/CoralTheorizing/YoinkMidPieceAuto.java
+++ src/main/java/frc/robot/commands/CoralTheorizing/YoinkMidPieceAuto.java
@@ -1,32 +1,32 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.CoralTheorizing;
+// package frc.robot.commands.CoralTheorizing;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.path.PathConstraints;
-import com.pathplanner.lib.path.PathPlannerPath;
+// import com.pathplanner.lib.auto.AutoBuilder;
+// import com.pathplanner.lib.commands.PathPlannerAuto;
+// import com.pathplanner.lib.path.PathConstraints;
+// import com.pathplanner.lib.path.PathPlannerPath;
 
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.SwerveSubsystem;
+// import edu.wpi.first.math.util.Units;
+// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+// import frc.robot.subsystems.SwerveSubsystem;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class YoinkMidPieceAuto extends SequentialCommandGroup {
-  /** Creates a new YoinkMidPieceAuto. */
-  public YoinkMidPieceAuto(SwerveSubsystem swerve) {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(
-      new PathPlannerAuto("MoveToMid"),
-      new MoveToGamePieceCommand(swerve),
-      AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("GoToSpeaker"), 
-              new PathConstraints(3, 5, Units.degreesToRadians(540), Units.degreesToRadians(720)))
+// // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// // information, see:
+// // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+// public class YoinkMidPieceAuto extends SequentialCommandGroup {
+//   /** Creates a new YoinkMidPieceAuto. */
+//   public YoinkMidPieceAuto(SwerveSubsystem swerve) {
+//     // Add your commands in the addCommands() call, e.g.
+//     // addCommands(new FooCommand(), new BarCommand());
+//     addCommands(
+//       new PathPlannerAuto("MoveToMid"),
+//       new MoveToGamePieceCommand(swerve),
+//       AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("GoToSpeaker"), 
+//               new PathConstraints(3, 5, Units.degreesToRadians(540), Units.degreesToRadians(720)))
 
-    );
-  }
-}
+//     );
+//   }
+// }
