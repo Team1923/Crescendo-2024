d09421a79f9cffa795863a66cf26cf59f876c9d5
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2024-02-16 14:36:17 -0500

    override on intake deploy

--- src/main/java/frc/robot/commands/Intake/DeployIntakeCommand.java
+++ src/main/java/frc/robot/commands/Intake/DeployIntakeCommand.java
@@ -12,6 +12,7 @@ import frc.lib.RobotStateUtils.StateVariables.IntakeStates;
 
 public class DeployIntakeCommand extends Command {
   StateHandler stateHandler = StateHandler.getInstance();
+  boolean allowedToGoUp = false;
   /** Creates a new DeployIntakeCommand. */
   public DeployIntakeCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -28,7 +29,11 @@ public class DeployIntakeCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    if (stateHandler.getBBOneCovered() && !stateHandler.getBBThreeCovered()) {
+      allowedToGoUp = false;
+    } else if (!stateHandler.getBBOneCovered() && stateHandler.getBBThreeCovered()) {
+      allowedToGoUp = true;
+    }
   }
 
   // Called once the command ends or is interrupted.
@@ -42,6 +47,6 @@ public class DeployIntakeCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return stateHandler.getBBThreeCovered();
+    return stateHandler.getBBThreeCovered() && allowedToGoUp;
   }
 }
