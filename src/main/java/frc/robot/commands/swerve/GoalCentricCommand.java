9c953e3b4948d1017d06a3933fa9051ddab0f41a
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2024-02-22 14:37:52 -0500

    template for goal centric

---
+++ src/main/java/frc/robot/commands/swerve/GoalCentricCommand.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.swerve;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class GoalCentricCommand extends Command {
+  private CommandSwerveDrivetrain swerve;
+  private SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric();
+  /** Creates a new GoalCentricCommand. */
+  public GoalCentricCommand(CommandSwerveDrivetrain s) {
+    swerve = s;
+    addRequirements(swerve);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
