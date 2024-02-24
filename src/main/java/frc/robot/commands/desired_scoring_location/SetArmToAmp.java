f07d87f9fa55e45984e63a81b750507abdffee4d
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2024-02-23 22:29:37 -0500

    maybe file fixed

--- src/main/java/frc/robot/commands/desired_scoring_location/SetArmToAmp.java
+++ src/main/java/frc/robot/commands/desired_scoring_location/SetArmToAmp.java
@@ -1,8 +1,8 @@
+package frc.robot.commands.Desired_Scoring_Location;
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
-package frc.robot.commands.desired_scoring_location;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.lib.StateMachine.StateHandler;
