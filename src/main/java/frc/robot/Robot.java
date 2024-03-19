a5addd69e8583172c6e1b5a74d4d0ae004511298
S1OLY <shubaniyer@gmail.com>
2024-03-19 17:26:43 -0400

    removed timer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -47,7 +47,7 @@ public class Robot extends TimedRobot {
   private DigitalInput outsideButton = new DigitalInput(ButtonConstants.outsideButton);
   private DigitalInput insideButton = new DigitalInput(ButtonConstants.insideButton);
 
-  private Timer bb1Timer = new Timer();
+
 
 
 
@@ -112,12 +112,7 @@ public class Robot extends TimedRobot {
     m_autonomousCommand = m_robotContainer.initializeAuto(selector);
     LimelightInterface.getInstance().aprilTagFieldLayout.setOrigin(OriginPosition.kBlueAllianceWallRightSide);
     if(StateHandler.getInstance().getBBOneCovered()){
-      bb1Timer.start();
-    }
-    if(bb1Timer.get() > 0.3 && StateHandler.getInstance().getBBOneCovered()){
       StateHandler.getInstance().setNoBB1(true);
-      bb1Timer.stop();
-      bb1Timer.reset();
     }
 
     if (m_autonomousCommand != null) {
@@ -132,8 +127,6 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousExit() {
     StateHandler.getInstance().setAutoOverride(false);
-    bb1Timer.stop();
-    bb1Timer.reset();
   }
 
   @Override
